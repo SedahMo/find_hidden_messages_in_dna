b45a1362856bf61b1127ba09f6ac0a9b22007bc7
SedahMo <babemomo@vip.qq.com>
2021-01-14 07:03:53 -0500

    Add week4 code

---
+++ week4code/RandomizedMotifSearch.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 14 00:53:56 2021
+
+@author: babemomo
+"""
+import random as rd
+import W4library as w4
+import time
+
+
+def RandomizedMotifSearch(dna_list, k, t):
+    """Hi."""
+    start = True
+    for i in range(1000):
+        ran_list = [rd.randint(0, len(dna_list[0]) - k) for i in range(t)]
+        iter_seqs = [seq[ran_list[i]: ran_list[i] + k]
+                     for i, seq in enumerate(dna_list)]
+        if start:
+            best_seqs = iter_seqs[:]
+            start = False
+        while True:
+            profile = w4.ProfileMatrix(iter_seqs, k, t, mode='profile')
+            iter_seqs = [w4.ProfileMostPKmer(seq, k, profile)
+                         for seq in dna_list]
+            if w4.ProfileMatrix(iter_seqs, k, t, mode='score') < \
+               w4.ProfileMatrix(best_seqs, k, t, mode='score'):
+                best_seqs = iter_seqs[:]
+            else:
+                break
+    print(w4.ProfileMatrix(best_seqs, k, t, mode='score'))
+    return best_seqs
+
+
+if __name__ == "__main__":
+    with open('dataset_161_5.txt') as f:
+    # with open(input('Please input the path and press enter: \n')) as f:
+        line_list = f.read().splitlines()
+        params = line_list.pop(0).split()
+        k = int(params[0])
+        t = int(params[1])
+        dna_list = line_list
+    start = time.process_time()
+    print(*RandomizedMotifSearch(dna_list, k, t), sep='\n')
+    end = time.process_time()
+    print("Time:", end - start)
+
+
+# < 15.5, low-non-pass 15.9 high-pass 15.4
---
+++ week4code/W4library.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 14 00:53:00 2021
+
+@author: babemomo
+"""
+import numpy as np
+
+
+rows = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+
+
+def ProfileMatrix(kmer_list, k, t, mode):  # nrow = t, ncol = k
+    np_matrix = np.zeros((4, k))  # matrix.shape = 4, k
+    for col, colbases in enumerate(zip(*kmer_list)):
+        for letter in 'ACGT':
+            np_matrix[rows[letter], col] = colbases.count(letter)
+    pseudo_count = 0.1
+    if mode == 'profile':
+        np_matrix += 1 * pseudo_count
+        return np_matrix / (t + 4 * pseudo_count)
+    # if mode == 'score':
+    #     return t * k - sum((np.amax(np_matrix, axis=0)))
+    if mode == 'score':
+        np_matrix += 1 * pseudo_count
+        np_matrix /= (t + 4 * pseudo_count)
+        return np.sum(-np_matrix * np.log2(np_matrix))
+    if mode == 'entropy':
+        np_matrix /= t
+        np_matrix = np_matrix.reshape(-1)
+        np_matrix = np_matrix[np_matrix != 0]
+        return np.sum(-np_matrix * np.log2(np_matrix))
+
+
+def ProfileMostPKmer(sequence, k, np_matrix):
+    max_p = 0
+    motif = []
+    for i in range(len(sequence) - k + 1):
+        p = 1
+        kmer = sequence[i: (i + k)]
+        for col, letter in enumerate(kmer):
+            p *= np_matrix[rows[letter], col]
+        if p > max_p:
+            max_p = p
+            motif = [kmer]
+        elif p == max_p:
+            motif.append(kmer)
+    return motif[0]
